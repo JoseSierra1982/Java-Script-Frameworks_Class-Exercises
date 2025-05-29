c8fef781f59acf59e4ac4df22c71cf002c9a7618
Jose David Sierra <82597748+JoseSierra1982@users.noreply.github.com>
2025-05-29 11:53:16 -0400

    Update Lab1.js

--- Lab1.js
+++ Lab1.js
@@ -1,9 +1,9 @@
 import fs from 'node:fs';                           
-const data = fs.readFile('./week_3/temp.txt','utf8',(error,data)=>{
+const data = fs.readFile('./temp.txt','utf8',(error,data)=>{
 
   if (error)
     {
-        console.log(wrong_file);
+        console.log(error);
     }
   else  
     {   

98cfdcc8a9c1bac8527485a9805774662f3ffd76
Jose David Sierra R <ingenierojd2012@hotmail.com>
2025-05-29 11:13:05 -0400

    Initial commit

---
+++ week_1/script2.js
@@ -0,0 +1,16 @@
+// Functions/Method:
+//Methods are functions that are written inside a class
+
+function add(x,y) {
+    return x+y;
+}
+
+add(5,6);
+
+let add2 = function ad2(x,y) {
+    return x+y;
+}
+
+add2(5,6);
+
+console.log(add)
---
+++ week_2/script2.js
@@ -0,0 +1,6 @@
+//we can start with node.js
+//I want to test/run this using node.js
+//ctrl+
+
+console.log(Process)
+

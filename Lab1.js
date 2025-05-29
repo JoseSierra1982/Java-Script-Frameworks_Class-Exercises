f244e9ccbe8d5f2401e3f9c991a7fd36aad0893c
Jose David Sierra R <ingenierojd2012@hotmail.com>
2025-05-29 12:44:38 -0400

    Add Lab1.js

--- Lab1.js
+++ Lab1.js
@@ -1,6 +1,6 @@
 
 import fs from 'node:fs';                           
-const data = fs.readFile('./temp.txt','utf8',(error,data)=>{
+const data = fs.readFile('./text.txt','utf8',(error,data)=>{
 
   if (error)
     {
